0c4c4f71c4a0a9eff8100884874454f38da99d74
mdipanjan <mdipanjan33@gmail.com>
2019-11-19 03:41:07 -1200

    Updates

---
+++ precache-manifest.c6005d5c7ba243556420a3e4f8d12000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf304029673e6edd5d862e9bf0cd2cb1",
+    "url": "/tulip/index.html"
+  },
+  {
+    "revision": "5ab41091ab2b7962d1f4",
+    "url": "/tulip/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "4632aca70c9532bbf2a1",
+    "url": "/tulip/static/css/main.c7009cc0.chunk.css"
+  },
+  {
+    "revision": "5ab41091ab2b7962d1f4",
+    "url": "/tulip/static/js/2.a3c6f4de.chunk.js"
+  },
+  {
+    "revision": "4632aca70c9532bbf2a1",
+    "url": "/tulip/static/js/main.ca0e66b3.chunk.js"
+  },
+  {
+    "revision": "4ce0db1fed3be457497f",
+    "url": "/tulip/static/js/runtime-main.dfd8f139.js"
+  }
+]);

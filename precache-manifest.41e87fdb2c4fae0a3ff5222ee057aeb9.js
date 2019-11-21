ca43791eac5da038b43166e6d38bd807f8f3f59b
mdipanjan <mdipanjan33@gmail.com>
2019-11-21 07:40:58 -1200

    Updates

---
+++ precache-manifest.41e87fdb2c4fae0a3ff5222ee057aeb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "177794a9e8b92bea93f1b3aa7bc4450f",
+    "url": "/tulip/index.html"
+  },
+  {
+    "revision": "06b8cc8e1224156ab1d6",
+    "url": "/tulip/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "95fe63181b940e1787c9",
+    "url": "/tulip/static/css/main.92da9d62.chunk.css"
+  },
+  {
+    "revision": "06b8cc8e1224156ab1d6",
+    "url": "/tulip/static/js/2.a07b57f1.chunk.js"
+  },
+  {
+    "revision": "95fe63181b940e1787c9",
+    "url": "/tulip/static/js/main.fd7f1647.chunk.js"
+  },
+  {
+    "revision": "4ce0db1fed3be457497f",
+    "url": "/tulip/static/js/runtime-main.dfd8f139.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tulip/precache-manifest.7aa51097c82c0548221b376cbdc17b8d.js"
+  "/tulip/precache-manifest.41e87fdb2c4fae0a3ff5222ee057aeb9.js"
 );
 
 self.addEventListener('message', (event) => {

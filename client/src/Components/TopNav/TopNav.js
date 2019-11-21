06cb1633586ec8a34030802153ae786c016a3731
mdipanjan <mdipanjan33@gmail.com>
2019-11-21 05:46:11 -1200

    changes

--- client/src/Components/TopNav/TopNav.js
+++ client/src/Components/TopNav/TopNav.js
@@ -11,6 +11,8 @@ import {
   MdHelp,
   MdInfo
 } from "react-icons/md";
+import { FiFilePlus } from "react-icons/fi";
+
 import { NavLink } from "react-router-dom";
 import { FaStore, FaUserCircle } from "react-icons/fa";
 import { GoShield } from "react-icons/go";
@@ -20,6 +22,7 @@ class TopNav extends Component {
   expandMenu = e => {
     if (!e.target.classList.contains("open")) {
       let elem = document.querySelector(".sidebar");
+      document.querySelector('body').classList.add('overlay');
       elem.classList.add("expand");
     }
     console.log(e.target.classList.contains("open"));
@@ -27,88 +30,91 @@ class TopNav extends Component {
   closemenu = e => {
     let elem = document.querySelector(".sidebar");
     elem.classList.remove("expand");
+    document.querySelector('body').classList.remove('overlay');
   };
   render() {
     return (
       <div className="top-navigation">
-        <span onClick={this.expandMenu} className="menu-toggle ml-2">
+        <span onClick={this.expandMenu} className="menu-toggle ml-3 mt-1">
           <MdSubject className="toggle-icon" />
         </span>
         <div className="sidebar">
-          <div className="close-icon-box">
-            <span onClick={this.closemenu} className="close-icon mt-3 ml-2">
-              <MdClose className="close-icon-inner" />
+          <div className="profile-wrap">
+            <div className="close-icon-box">
+              <span onClick={this.closemenu} className="close-icon mt-4 ml-2">
+                <MdClose className="close-icon-inner" />
+              </span>
+            </div>
+            <div className="guest-box mt-4">
+            <span className="float-left ">
+              <FaUserCircle className="guest-icon mr-5"/>
             </span>
+            <p className="guest-label">Hello guest!</p>
           </div>
-          <div className="guest-box mt-4 ml-5">
-            <span className="float-left">
-              <FaUserCircle className="guest-icon mr-3"/>
-            </span>
-            <p className="guest-label ">Hello guest!</p>
           </div>
+
           <ul className="menu-options">
             <li className="my-3 pl-2">
               <NavLink className="link" to="/home">
-                <span>
-                  <MdAssignmentTurnedIn className="link-icon mb-1" />
+                <span className="">
+                  <FiFilePlus className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text  ml-2">My appointments</span>
               </NavLink>
             </li>
-            <li className="my-3 pl-2">
+            <li className="my-4 pl-2">
               <NavLink className="link" to="/ome">
                 <span>
-                  <MdAssignment className="link-icon  mb-1" />
+                  <MdAssignment className="link-icon  mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">Lab tests</span>
               </NavLink>
             </li>
-            <li className="my-3 pl-2">
+            <li className="my-4 pl-2">
               <NavLink className="link" to="/home">
                 <span>
-                  <MdShoppingCart className="link-icon mb-1" />
+                  <MdShoppingCart className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">My orders</span>
               </NavLink>
             </li>
-            <li className="my-3 pl-2">
+            <li className="my-4 pl-2">
               <NavLink className="link" to="/health-insurances">
                 <span>
-                  <GoShield className="link-icon mb-1" />
+                  <GoShield className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">Insurance plus</span>
               </NavLink>
             </li>
-            <li className="my-3 pl-2">
+            <li className="my-4 pl-2">
               <NavLink className="link" to="/health-records">
                 <span>
-                  <MdLock className="link-icon mb-1" />
+                  <MdLock className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">Health record vault</span>
               </NavLink>
             </li>
-
-            <li className="my-3 pl-2">
+            <li className="my-4 pl-2">
               <NavLink className="link" to="/blood-finder">
                 <span>
-                  <GiDrop className="link-icon mb-1" />
+                  <GiDrop className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">Blood finder</span>
               </NavLink>
             </li>
-            <li className="my-3 pl-2">
+            <li className="my-4 pl-2">
               <NavLink className="link" to="/book-medicines">
                 <span>
-                  <FaStore className="link-icon mb-1" />
+                  <FaStore className="link-icon mb-1 mr-3" />
                 </span>
-                <span className="link-text ml-2 mb-4">Jan asudhi finder</span>
+                <span className="link-text ml-2 mb-4">Book medicines</span>
               </NavLink>
             </li>
             <hr  className="mr-4"/>
             <li className="my-3 pl-2">
               <NavLink className="link" to="/home">
                 <span>
-                  <MdHelp className="link-icon mb-1" />
+                  <MdHelp className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">Help</span>
               </NavLink>
@@ -116,7 +122,7 @@ class TopNav extends Component {
             <li className="my-3 pl-2">
               <NavLink className="link" to="/home">
                 <span>
-                  <MdSettings className="link-icon mb-1" />
+                  <MdSettings className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">Settings</span>
               </NavLink>
@@ -124,7 +130,7 @@ class TopNav extends Component {
             <li className="my-3 pl-2">
               <NavLink className="link" to="/home">
                 <span>
-                  <MdInfo className="link-icon mb-1" />
+                  <MdInfo className="link-icon mb-1 mr-3" />
                 </span>
                 <span className="link-text ml-2">About us</span>
               </NavLink>
--- client/src/views/BookMedicine/BookMedicine.js
+++ client/src/views/BookMedicine/BookMedicine.js
@@ -1,43 +1,60 @@
 import React, { Component } from "react";
 import "./BookMedicine.css";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { FaBriefcaseMedical , FaBoxOpen} from "react-icons/fa";
+
+
+
 export default class BookMedicine extends Component {
   render() {
     return (
       <div className="container py-5">
-        <div class="sc-box">
-          <div class="sc-container">
-            <h1 class="sc-main-title">How It Works</h1>
-            <div class="sc-searchbox">
-              <input required type="text" class="sc-searchbox-field" />
-              <div class="sc-searchbox-placeholder">
-                <i class="sc-searchbox-placeholder-icon fas fa-search"></i>
-                <p class="sc-searchbox-placeholder-text">Search an event</p>
-              </div>
-            </div>
+        {/* <h1 class="sc-main-title">How It Works</h1>
+        <div class="sc-searchbox">
+          <input required type="text" class="sc-searchbox-field" />
+          <div class="sc-searchbox-placeholder">
+            <i class="sc-searchbox-placeholder-icon fas fa-search"></i>
+            <p class="sc-searchbox-placeholder-text">Search Medicines</p>
           </div>
-          <div class="sc-container sc-timeline">
-            <div class="sc-timeline-item" event="launch">
-              <i class="sc-timeline-icon fas fa-utensils"></i>
-              <div class="sc-timeline-info">
-                <h3 class="sc-timeline-title">Order Medication</h3>
+        </div> */}
+        {/* <h1 class="">How It Works</h1> */}
+        <div className="how-works-box">
+          <div class="sc-box">
+            <div class="sc-container sc-timeline">
+              <div class="sc-timeline-item" event="launch">
+                <i class="sc-timeline-icon fas fa-utensils">
+                <IoLogoWhatsapp/>
+                </i>
+                <div class="sc-timeline-info">
+                  <h3 class="sc-timeline-title">
+                      Drop Your Prescription
+                  </h3>
+                  <span className="side-text">Whatsapp your prescription to + 91 8424445751</span>
+                </div>
               </div>
-            </div>
-            <div
-              class="sc-timeline-item sc-actionsheet-trigger"
-              event="meeting"
-              goto="meeting"
-            >
-              <i class="sc-timeline-icon fas fa-comments"></i>
-              <div class="sc-timeline-info">
-                <h3 class="sc-timeline-title">Choose Medicines</h3>
+              <div
+                class="sc-timeline-item sc-actionsheet-trigger"
+                event="meeting"
+                goto="meeting"
+              >
+                <i class="sc-timeline-icon fas fa-comments">
+                    <FaBriefcaseMedical/>
+                </i>
+                <div class="sc-timeline-info">
+                  <h3 class="sc-timeline-title">Choose Medicines</h3>
+                  <span className="side-text">Select medicines from the app or use whatsapp bot</span>
+                </div>
               </div>
-            </div>
-            <div class="sc-timeline-item" event="meeting">
-              <i class="sc-timeline-icon fas fa-comments"></i>
-              <div class="sc-timeline-info">
-                <h3 class="sc-timeline-title">
-                  Free Home Delivery
-                </h3>
+              <div class="sc-timeline-item" event="meeting">
+                <i class="sc-timeline-icon fas fa-comments">
+                  <FaBoxOpen/>
+                </i>
+                <div class="sc-timeline-info">
+                  <h3 class="sc-timeline-title">
+                    Free Home Delivery
+                  </h3>
+                  <span className="side-text">Refill and save reminders</span>
+                </div>
               </div>
             </div>
           </div>

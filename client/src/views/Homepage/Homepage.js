564cf3061291ef2bfc171ca1db791cbacd10c98a
mdipanjan <mdipanjan33@gmail.com>
2019-11-22 09:41:11 -1200

    changes

--- client/src/views/Homepage/Homepage.js
+++ client/src/views/Homepage/Homepage.js
@@ -21,6 +21,42 @@ export default class Homepage extends Component {
           />
           <GoSearch className="search-icon" />
         </div>
+        <div class="ourservices">
+          <div class="maincontainer">
+            <div class="row">
+              <div class="col-12">
+                <h3>Our Services</h3>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-12">
+                <div class="oservicesbox">
+                  <div class="oservicesboxinner">
+                    <h4>Chat With Our Doctors</h4>
+                    <p>
+                      Now distance is not a constrain to get health consultations, get doctors advise anytime anywhere.
+                    </p>
+                    <a class="bookbtn">Explore Now</a>
+                  </div>
+                  <img src="images/humberto-chavez.png" alt="Humberto Chavez" />
+                </div>
+                <div class="oservicesbox">
+                  <div class="oservicesboxinner">
+                    <h4>Subscription based pharmacy</h4>
+                    <p>
+                      Chat with doctors Chat with doctors Chat with doctors Chat
+                      with doctors Chat with doctors Chat with doctors
+                    </p>
+                    <a class="bookbtn">Book</a>
+                  </div>
+                  <div>
+                  <img src="images/medicine.png" alt="Humberto Chavez" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="consultation-box mb-3 pt-4">
           <label className="consult-label">Find doctors for consultation</label>
           <Slider />

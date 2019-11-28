e8bb7857000dfa94e4462e8a6bf99d9f5d5e1df0
mdipanjan <mdipanjan33@gmail.com>
2019-11-28 06:01:46 +0530

    bot-created

--- whatsapp-bot/index.js
+++ whatsapp-bot/index.js
@@ -3,6 +3,7 @@ const  cors = require('cors');
 const twilio = require('twilio');
 const app = express();
 const config = require('./config');
+const {google} = require('googleapis');
 app.use(express.json());
 
 app.use(cors());
@@ -12,13 +13,23 @@ app.use(
     extended: false
   })
 );
-async function googleSearch(req, res){
+async function googleSearch(req, res, next){
   const client = new twilio(config.twilioAccountSid, config.twilioAuthToken);
+  const customsearch = google.customsearch('v1');
   const { MessagingResponse } = twilio.twiml;
   const twiml = new MessagingResponse();
   const q = req.body.Body;
+  const cx = config.searchEngineId;
+  const googleApiKey =  config.customSearchApi
+  const options = { cx, q, auth: googleApiKey };;
+
   try {
-    twiml.message(`helloworld`);
+    const result = await customsearch.cse.list(options);
+    const firstResult = result.data.items[0];
+    const searchData = firstResult.snippet;
+    const link = firstResult.link;
+
+    twiml.message(`${searchData} ${link}`);
 
     res.set('Content-Type', 'text/xml');
 
@@ -33,4 +44,4 @@ app.get('/', (req,res)=>{
   res.send('ok')
 })
 app.post('/', googleSearch);
-app.listen(8000, () => console.log(`Magic...Magic On => 8000!`))
+app.listen(8000, () => console.log(`listening on port 8000!`))

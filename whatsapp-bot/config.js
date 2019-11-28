5c729223fc6afd0d32983009505806d1b5c936e8
Dipanjan Mondal <38426196+mdipanjan@users.noreply.github.com>
2019-11-28 07:02:09 +0530

    Delete config.js

--- whatsapp-bot/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    twilioAccountSid:'AC056d83ae37b22231df1e5e77f5713745',
-    twilioAuthToken : '5588e6da560573c062625dca6be42d67',
-    twilioPhoneNumber:'+1455238886',
-    customSearchApi:'AIzaSyBlLHYyI7l_l5c9Duabkdfp06cAQ0NR3og',
-    searchEngineId:'018196883792985298714:gfnfcmohjtg'
-}
